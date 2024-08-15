aa12578dd56fb26b9ee7a4068cff902e7d251b38
24626982 <hank50380@gmail.com>
2024-08-16 00:28:42 +0800

    重新學習Vue3+TS，默認函式+變數轉換

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="app">
-    Hello,World!
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('KKK')">change name</button>
+    <button @click="changeAge(30)">change name</button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+    data(){
+      return{
+        name:'Hank' as string,
+        age: 22 as number | string,
+      }
+    },
+    methods:{
+      changeName(name: string) {
+        this.name = name
+      },
+      changeAge(age: number | string){
+        this.age = age
+      }
+    }
+  })
+</script>
 
 <style>
 
