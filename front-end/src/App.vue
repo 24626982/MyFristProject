eed806f55d108c594679b5d5ad74a3cd78b6fc4e
24626982 <hank50380@gmail.com>
2024-08-31 12:11:10 +0800

    做了一些改動，把循環撥放用成自己喜歡的金句，預計接下來開始處理後端

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -24,7 +24,7 @@
           連會議記錄都不做，就在一旁耍白癡，還以為自己很重要，真的很可悲
         </div>
         <div class="blog-article page-number">
-          NO. 01
+          NO. 00
         </div>
       </div>
       <div class="blog-header-container">
@@ -39,39 +39,12 @@
               alt="">
             <h2>Sharing The <span>Widespread</span> Acclaim About Motivation</h2>
             <div class="blog-detail">
-              <span>By Richard Carnation</span>
+              <span>By Hank</span>
               <span>5 Min Read</span>
             </div>
             <p>Blonde received widespread acclaim, with critics praising Ocean's introspective lyrics and the album's
               unconventional</p>
-            <a href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round" class="feather feather-corner-down-right"
-                viewBox="0 0 24 24">
-                <path d="M15 10l5 5-5 5" />
-                <path d="M4 4v7a4 4 0 004 4h12" />
-              </svg>
-              See More
-            </a>
-          </div>
-        </div>
-        <div class="blog-header">
-          <div class="blog-article header-article">
-            <div class="blog-big__title">Love</div>
-            <div class="blog-menu small-title date">12.06.2021</div>
-          </div>
-          <div class="blog-article">
-            <img
-              src="https://images.unsplash.com/photo-1529255484355-cb73c33c04bb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTh8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="">
-            <h2>Talking About <span>Vulnerability</span> During Quarantine</h2>
-            <div class="blog-detail">
-              <span>By Tom Hiddleston</span>
-              <span>5 Min Read</span>
-            </div>
-            <p>Having traveled to Turkey multiple times, with critics praising Ocean's introspective lyrics and the
-              album's unconventional</p>
-            <a href="#">
+            <a href="https://tw.manhuagui.com/list/japan/update.html"> <!--待修改-->
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2"
                 stroke-linecap="round" stroke-linejoin="round" class="feather feather-corner-down-right"
                 viewBox="0 0 24 24">
@@ -112,9 +85,11 @@
       </div>
       <div class="blog-part right-blog">
         <marquee width="100%" direction="left">
-          <span>Now And Then You Miss It Sounds Make You Cry</span>
-          <span>Now In - MoMa Sharing Exhibition NOW</span>
-          <span>NYC Opens After Long Lockdown Check</span>
+          <span>People call 'love', is just a chemical reaction that compels animal to breed. It hits hard, then it
+            slowly fades, leaving you stranded in a filling marriage.</span>
+          <span>To live is to risk it all. Otherwise, you are just an inert chunk of randomly assembled molecules,
+            drifting wherever the universe blows you</span>
+          <span>No union built on running from your problems lasts more than five years, seven tops</span>
         </marquee>
         <div class="blog-right-title-container">
           <div class="blog-right-title">
@@ -192,12 +167,12 @@
 import { defineComponent, reactive, ref, toRefs } from 'vue';
 
 export default defineComponent({
-  
+
   setup() {
     let today_Date: Date = new Date();
     let fromtime: string = today_Date.toLocaleString();
-    return {fromtime}
-    },
+    return { fromtime }
+  },
   methods: {
 
   },
