38ee8e21baab37ab3c25a48923ecdfd967a8795b
24626982 <hank50380@gmail.com>
2024-11-15 22:31:34 +0800

    前端框架放棄Vue改用React

--- front-end/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './assets/global.css'
-import './assets/Blog.scss'
-
-createApp(App).use(store).use(router).mount('#app')
